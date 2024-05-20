2c52f86269c57af6c7486026010da0b45cbe27c2
alexharris <Alexrlh2@gmail.com>
2024-05-20 19:20:51 +0100

    Display a hello world window

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from kivy.app import App
+from kivy.uix.label import Label
+
+class EcosystemApp(App):
+    def build(self):
+        return Label(text="Hello, World!")
+
+
+if __name__ == '__main__':
+    EcosystemApp().run()
+
