84a93faa55e7c3e1118a64e47de2a0c96b85f7e9
alexharris <Alexrlh2@gmail.com>
2024-06-09 21:01:08 +0100

    Give world method to get objects within radius
    All objects within a certiain radius can be fetched from world

--- src/world.py
+++ src/world.py
@@ -1,7 +1,14 @@
+from typing import List
+
 from herbivore import Herbivore
 from plant import Plant
 
+import geometry_utils
+
 import random
+import math
+
+
 class World:
     def __init__(self, width, height):
         self.width = width
@@ -15,6 +22,8 @@ class World:
         if object in self.game_objects:
             self.game_objects.remove(object)
 
+    def get_objects_near(self, position: tuple[float, float], radius: int) -> List:
+        return [object for object in self.game_objects if geometry_utils.is_closer_than(position, object.pos, radius)]
+
     def get_objects(self):
         return self.game_objects
-
